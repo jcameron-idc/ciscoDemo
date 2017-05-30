367154474b2ec0b01c9e0a28039cfd7132b60933
Jason Cameron <kiocam246@gmail.com>
2017-05-30 17:58:47 -0400

    version 1.1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 $(function(){
-    var e = document.forms['form1'];
+    var e = document.forms['form'];
     e = e.getElementsByTagName('label');
     var num2 = e.length;
 
-    var $checkboxes = $('#form1 input[type="checkbox"]');
+    var $checkboxes = $('#form1, #form2, #form3, input[type="checkbox"]');
 
     $checkboxes.change(function(){
         var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
-        $('#count-checked-checkboxes').text('Name of Section ' + countCheckedCheckboxes);
-
-        //$('#edit-count-checked-checkboxes').val(countCheckedCheckboxes);
+        $('#count-checked-checkboxes, #count-checked-checkboxes2, #count-checked-checkboxes3').text('Name of Section ' + countCheckedCheckboxes);
     });
 
-    $('#numOfQuestions').append( "/" + e.length);
-
+    $('#numOfQuestions, #numOfQuestions2, #numOfQuestions3').append( "/" + e.length);
 
 });
