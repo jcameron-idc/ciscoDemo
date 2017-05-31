a8d4f00a61066840e2a3e7b2892a92f8a1848f0d
Jason Cameron <kiocam246@gmail.com>
2017-05-31 14:20:11 -0400

    version2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-// function myFunction() {
-//     var x = document.forms[0];
-//     var txt = "";
-//     var i;
-//     for (i = 0; i < x.length; i++) {
-//         txt = txt + x.elements[i].value + "<br>";
-//     }
-//     document.getElementById("demo").innerHTML = txt;
-// }
-$(function () {
-    var e = document.forms[0];
-    var numQuestion;
-    var i;
-    var formName = $(this).e;
-    for (i = 0; i < e.length; i++) {
-        numQuestion = e.getElementsByTagName('label');
-        console.log(numQuestion.length);
-    }
 
-    $('#' + formName).closest('.panel-heading').find('.numOfQuestions').text('/' + e.length);
+// $(function () {
+//     var $form = $(this).closest('form');
+//     var formName = $form.attr('id');
+//
+//     // --- count the checkboxes for this form ---
+//     var totalCheckboxes = $form.children('input[type="checkbox"]').length;
+//
+//     // --- place count value in the 'numOfQuestions' p ---
+//     var $panel = $form.closest('.panel-default');
+//     $panel.find('.numOfQuestions').text('/' + totalCheckboxes);
+//
+//     //$(this).closest('.panel-heading').find('.numOfQuestions').text('/' + e.length);
+//
+//     var $checkboxes = $('input[type="checkbox"]').on('change', function() {
+//         var count = $(this).closest('.panel-body').find('input:checked').length;
+//         $(this).closest('.panel').find('.count-checked-checkboxes').text('Name of Section ' + count);
+//     });
+//
+//     // $checkboxes.change(function () {
+//     //     var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
+//     //     $(this).siblings('.panel-heading').find('.count-checked-checkboxes').text('Name of Section ' + countCheckedCheckboxes);
+//     //     // $('#count-checked-checkboxes, #count-checked-checkboxes2, #count-checked-checkboxes3').text('Name of Section ' + countCheckedCheckboxes);
+//     // });
+// });
 
-    var $checkboxes = $('input[type="checkbox"], #form1');
 
-    $checkboxes.change(function () {
-        var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
-        $(this).siblings('.panel-heading').find('.count-checked-checkboxes').text('Name of Section ' + countCheckedCheckboxes);
-        // $('#count-checked-checkboxes, #count-checked-checkboxes2, #count-checked-checkboxes3').text('Name of Section ' + countCheckedCheckboxes);
-    });
+
+// --- when user changes a checkbox ---
+$(document).on('change', 'input[type="checkbox"]', function(e) {
+    // --- get the form name ---
+    var $form = $(this).closest('form');
+    var formName = $form.attr('id');
+
+    // --- count the checkboxes for this form ---
+    var totalCheckboxes = $form.children('input[type="checkbox"]').length;
+
+    // --- place count value in the 'numOfQuestions' p ---
+    var $panel = $form.closest('.panel-default');
+    $panel.find('.numOfQuestions').text('/' + totalCheckboxes);
+
+    // --- count total checkboxes checked ---
+    var countCheckedCheckboxes = $form.children('input[type="checkbox"]:checked').length;
+
+    // --- place checked count in 'count-checked-checkboxes' p ---
+    $panel.find('.count-checked-checkboxes').text('Name of Section '+countCheckedCheckboxes);
+
 });
